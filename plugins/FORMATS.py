935af3121e818d1c458cd8d7057036521e72f476
Krishna <kd259457@gmail.com>
2025-12-08 16:05:37 +0530

    Update FORMATS.py

--- plugins/FORMATS.py
+++ plugins/FORMATS.py
@@ -45,8 +45,8 @@ HELP_TEXT = """<b>⁉️ Hᴇʟʟᴏ {mention} ~
 <b><i>◈ Sᴛɪʟʟ ʜᴀᴠᴇ ᴅᴏᴜʙᴛs, ᴄᴏɴᴛᴀᴄᴛ ʙᴇʟᴏᴡ ᴘᴇʀsᴏɴs/ɢʀᴏᴜᴘ ᴀs ᴘᴇʀ ʏᴏᴜʀ ɴᴇᴇᴅ !</i></b>"""
 
 ABOUT_TXT = """<b>🤖 ᴍʏ ɴᴀᴍᴇ: {botname}
-<blockquote expandable>›› ᴏᴡɴᴇʀ: <a href='tg://openmessage?user_id=6093336723'>ʜᴜɴᴛᴇʀ</a>
-›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/hunter_invincible'>ʜᴜɴᴛᴇʀ</a></b></blockquote>"""
+<blockquote expandable>›› ᴏᴡɴᴇʀ: <a href='tg://openmessage?user_id=8053451072'>ʜᴜɴᴛᴇʀ</a>
+›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/Hunter_1006'>ʜᴜɴᴛᴇʀ</a></b></blockquote>"""
 
 SETTING_TXT = """<b>⚙️ Cᴏɴғɪɢᴜʀᴀᴛɪᴏɴs</b>
 <blockquote expandable>◈ ᴛᴏᴛᴀʟ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ:  <b>{total_fsub}</b>
